c19491cf975b3ec814555331f88d1a2ca0b36cce
ria yahshua <yahshua.abuemeri@gmail.com>
2025-02-26 09:47:43 +0800

    WIP: Add Job Page

--- vue-traversy/vite.config.js
+++ vue-traversy/vite.config.js
@@ -5,6 +5,15 @@ import {resolve} from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy:{
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/,'')
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
